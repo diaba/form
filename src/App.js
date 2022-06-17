a5109327f6cc90e90f1a34560c13699961abc78c
diaba <hwar@cincinnatistate.edu>
2022-06-17 12:34:28 -0400

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+import React from "react";
+import EditEmployee from "./EditEmployee";
+class App extends React.Component {
+  constructor() {
+    super();this.state ={ 
+      nameError:'',
+      message:'test',
+      isValid:true
+    }
+  }
+  validateName = (event) => {
+    var name = event.target.value;
+    if(name.length < 5){
+      this.setState({ nameError:"Name is invalid"})
+    }else{
+      this.setState({ nameError:''})
+      this.setState({isValid:true})
+    }
+  };
+  sendData=(event)=>{
+    event.preventDefault()
+   this.setState({ message:'Send with succes'})
+  }
+  render() {
+    return (
+      // <form onSubmit={this.sendData}>
+      //   <input onChange={this.validateName} type="text" name="name" /><span>{this.state.nameError}</span> <br /> <br />
+        
+      //   <input type="text" name="name" />
+      //   <span></span>
+      //   <br /> <br />
+      //   <button type="submit"  disabled ={!this.state.isValid}>Register</button>
+      //   <p><strong>{this.message}</strong></p>
+      // </form>
+      <EditEmployee />
+    );
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,150 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import "./App.css";
+class AddBook extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      title: "",
+      author: "",
+      genre: "",
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+      formErrors: {
+        titleErr: "",
+        authorErr: "",
+        genreErr: "Please select a genre",
+      },
+      fieldValidity: {
+        title: false,
+        author: false,
+        genre: false,
+      },
+      formValid: false,
+      successMessage: "",
+    };
+    this.validateTitle = (e) => {
+      const name = e.target.value;
+      var formErrors = this.state.formErrors;
+      var fieldValidity = this.state.fieldValidity;
+      this.setState({ title: e.target.value });
+      if (name.length < 4) {
+        formErrors.titleErr = "Name must contain at least 4 characters";
+        fieldValidity.title = false;
+      } else {
+        formErrors.titleErr = "";
+        fieldValidity.title = true;
+      }
+      this.setState({ fieldValidity: fieldValidity });
+      this.setState({
+        formValid:
+          fieldValidity.title && fieldValidity.author && fieldValidity.genre,
+      });
+    };
+    this.validateAuthor = (e) => {
+      const name = e.target.value;
+      var formErrors = this.state.formErrors;
+      var fieldValidity = this.state.fieldValidity;
+      this.setState({ author: e.target.value });
+      if (name.length < 3) {
+        formErrors.authorErr = "Name must contain at least 3 characters";
+        fieldValidity.author = false;
+      } else {
+        formErrors.authorErr = "";
+        fieldValidity.author = true;
+      }
+      this.setState({ fieldValidity: fieldValidity });
+      this.setState({
+        formValid:
+          fieldValidity.title && fieldValidity.author && fieldValidity.genre,
+      });
+    };
+
+    this.validateGenre = (e) => {
+      const name = e.target.value;
+      var formErrors = this.state.formErrors;
+      var fieldValidity = this.state.fieldValidity;
+      this.setState({ author: e.target.value });
+      if (name !== e.target[0].value) {
+        formErrors.genreErr = "";
+        fieldValidity.genre = true;
+      } else {
+        formErrors.genreErr = "Please select a genre";
+        fieldValidity.genre = false;
+      }
+      this.setState({ fieldValidity: fieldValidity });
+      this.setState({
+        formValid:
+          fieldValidity.title && fieldValidity.author && fieldValidity.genre,
+      });
+    };
+
+    this.validateSalary = (e) => {
+      let salary = e.target.value;
+      this.setState({ salary: salary });
+    };
+
+    this.add = (e) => {
+      e.preventDefault();
+      if (this.state.formValid) {
+        var formJSON = {
+          title: this.state.title,
+          author: this.state.author,
+          genre: this.state.genre,
+        };
+        console.log(JSON.stringify(formJSON));
+        this.setState({ successMessage: JSON.stringify(formJSON) });
+      }
+    };
+  }
+
+  render() {
+    return (
+      <div style={{ width: 500, margin: "0px auto" }}>
+        <h3 className="text-center">Add a Book</h3>
+        <form>
+          <div className="form-group">
+            <label>Title</label>
+            <input className="form-control" onChange={this.validateTitle} />
+          </div>
+          <span className="text-danger">{this.state.formErrors.titleErr}</span>
+          <div className="form-group">
+            <label>Author:</label>
+            <input className="form-control" onChange={this.validateAuthor} />
+          </div>
+          <span className="text-danger">{this.state.formErrors.authorErr}</span>
+          <div className="form-group">
+            <label>Genre:</label>
+            <select className="form-control" onChange={this.validateGenre}>
+              <option>--Select--</option>
+              <option value="Fiction">Fiction</option>
+              <option value="NoFiction">NoFiction</option>
+              <option value="Adventure">Adventure</option>
+              <option value="Science">Science</option>
+            </select>
+          </div>
+          <div>
+            <span className="text-danger">
+              {this.state.formErrors.genreErr}
+            </span>
+          </div>
+
+          <button
+            type="button"
+            disabled={!this.state.formValid}
+            className="btn btn-success mt-3"
+            onClick={this.add}
+          >
+            Add Book
+          </button>
+          <br />
+          <p>{this.state.successMessage}</p>
+        </form>
+      </div>
+    );
+  }
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<AddBook />);
